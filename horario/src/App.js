6481870dec277316593e8c1f34d396e0311878b9
Gabryel <gabryelpro260406@gmail.com>
2024-11-09 00:28:08 +0300

    terminado

--- horario/src/App.js
+++ horario/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Horario from "./componetes/estilo.css"
+export default function App(){
+  return(
+    <main>
+      <section>
+        <p>{new Date().toLocaleTimeString()}</p>
+      </section>
+    </main>
+  )
 }
-
-export default App;
